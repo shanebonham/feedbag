a445bd7cb8295ceb2c5ba1ec270da92a05990fc4
Shane Bonham <shanebonham@gmail.com>
2011-12-17 15:25:49 -0800

    Got rating view working

--- feedbag.rb
+++ feedbag.rb
@@ -5,3 +5,9 @@ set :haml, { :attr_wrapper => "\"" }
 get '/' do
   haml :home
 end
+
+get '/rating/:request_id/:timestamp' do
+  @request_id = params[:request_id]
+  @timestamp = params[:timestamp]
+  haml :rating
+end
