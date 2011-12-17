f61617e4278f24d3334a2b1f3d9f78b3b260ceb7
Shane Bonham <shanebonham@gmail.com>
2011-12-16 20:25:22 -0800

    Not using rails

--- app/models/rating.rb
+++
@@ -1,2 +0,0 @@
-class Rating < ActiveRecord::Base
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Feedbag::Application.config.secret_token = 'a6b7b1ad5f37e86afaded8a76c0119e4a97d8422be155c424b5e13a8c0c17084469688c0d005c08165698ff09828d2f9ab26cb0a5859a27b390197826f3826c1'
