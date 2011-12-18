0068992815c91dee38e71a0b94c86dff91f1a527
Shane Bonham <shanebonham@gmail.com>
2011-12-17 17:35:44 -0800

    First foray into communicating with the HelpSpot APIa

--- feedbag.rb
+++ feedbag.rb
@@ -1,13 +1,24 @@
 require 'sinatra'
+require 'help_spot'
 
-set :haml, { :attr_wrapper => "\"" }
+set :haml, { :attr_wrapper => '"' }
+
+help_spot = HelpSpot.new 'http://support.monkdevelopment.com/api/', 'shane@monkdevelopment.com', 'eYTjj7Z4Ec'
 
 get '/' do
   haml :home
 end
 
-get '/rating/:request_id/:timestamp' do
-  @request_id = params[:request_id]
+get '/rating/:accesskey/:timestamp' do
+  @accesskey = params[:accesskey]
+  @request = @accesskey[0,5]
+  @request_password = @accesskey[5,5]
   @timestamp = params[:timestamp]
+  @dump = help_spot.request @request
+  @request_history = @dump.request_history
+  @auth = "sadly, no"
+  if help_spot.authenticated?
+    @auth = "HELL YES!"
+  end
   haml :rating
 end
