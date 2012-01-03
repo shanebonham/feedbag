bf491f2eab577b24146792ee57437498a403c2d7
Shane Bonham <shanebonham@gmail.com>
2012-01-03 13:10:26 -0800

    First pass at feedback form.

--- feedbag.rb
+++ feedbag.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'help_spot'
 require 'data_mapper'
+require 'haml'
 
 set :haml, { :attr_wrapper => '"' }
 
@@ -10,7 +11,6 @@ class Rating
   include DataMapper::Resource
   property :id, Serial
   property :rating, Enum[:good, :okay, :bad]
-  property :timestamp, String
   property :agent, String
   property :request, String
   property :request_password, String
@@ -21,26 +21,26 @@ end
 
 DataMapper.finalize.auto_upgrade!
 
-helpers do
-  def get_reply(request, timestamp)
-    help_spot = HelpSpot.new 'http://support.monkdevelopment.com/api/', 'shane@monkdevelopment.com', 'eYTjj7Z4Ec'
-    response = help_spot.request request
-    request_history = response.request_history
-    stuff = ''
-    request_history.each do |x|
-      if (x.dtGMTChange.delete(' ') == timestamp.delete(' ')) && !x.tLog
-        stuff = x.xRequestHistory
-      end
-    end
-    return stuff
-  end
-end
+# helpers do
+#   def get_reply(request, timestamp)
+#     help_spot = HelpSpot.new 'http://support.monkdevelopment.com/api/', 'shane@monkdevelopment.com', 'eYTjj7Z4Ec'
+#     response = help_spot.request request
+#     request_history = response.request_history
+#     stuff = ''
+#     request_history.each do |x|
+#       if (x.dtGMTChange.delete(' ') == timestamp.delete(' ')) && !x.tLog
+#         stuff = x.xRequestHistory
+#       end
+#     end
+#     return stuff
+#   end
+# end
 
 get '/' do
   haml :home
 end
 
-get '/rating/:rating/:accesskey/' do
+get '/rate/:accesskey/:rating' do
   @accesskey = params[:accesskey]
   r = Rating.new
   r.rating = params[:rating]
@@ -48,5 +48,14 @@ get '/rating/:rating/:accesskey/' do
   r.request_password = @accesskey[5,5]
   r.created_at = Time.now
   r.save
-  haml :rating
+  redirect "/rating/#{@accesskey}/#{r.id}"
+end
+
+get '/rating/:accesskey/:id' do
+  @rating = Rating.get params[:id]
+  haml :feedback
+end
+
+put '/rating/:id' do
+  r = Rating.get params[:id]
 end
